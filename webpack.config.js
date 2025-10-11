034bcbef5cddb225f41d4c25119f642125997dea
KaiqueFJ21 <floreskaique@outlook.com.br>
2025-10-11 18:45:48 -0300

    fix webpack to deploy html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -50,5 +51,10 @@ export default {
   },
   resolve: {
     extensions: [".js", ".jsx"],
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })
+  ]
 };
