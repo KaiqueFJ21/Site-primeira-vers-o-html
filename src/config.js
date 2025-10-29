0fd57956f171de1a2005238a4cc5767201153ba6
KaiqueFJ21 <seu_email_real@dominio.com>
2025-10-28 21:40:06 -0300

    Correção 2

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+  API_URL: 'https://site-primeira-vers-o-html-6xwm.vercel.app/api'
+};
+
+export default config;
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,16 @@
-import path from "path";
-import { fileURLToPath } from "url";
+import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
-    clean: true
-  },
-  devServer: {
-    static: { directory: path.join(__dirname, "public") },
-    historyApiFallback: true,
-    port: 5173,
-    open: true,
-    proxy: [
-      {
-        context: ["/api"],
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        secure: false
-      }
-    ]
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -33,28 +18,31 @@ export default {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       },
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      '@config': path.resolve(__dirname, 'src/config.js')
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'public/index.html'
+      template: './public/index.html'
     })
-  ]
+  ],
+  devServer: {
+    historyApiFallback: true,
+    hot: true
+  }
 };
