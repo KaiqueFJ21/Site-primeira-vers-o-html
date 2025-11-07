a6cd87c73b2025b91f1ab154565df9ff8be4e3fa
KaiqueFJ21 <seu_email_real@dominio.com>
2025-11-07 20:51:48 -0300

    feat: adiciona todas as páginas e rotas no App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Contato from './pages/Contato';
+import Sobre from './pages/Sobre';
+import Servicos from './pages/Servicos';
 import './styles/global.css';
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <nav>
+        <nav className="navbar">
           <Link to="/">Home</Link>
+          <Link to="/sobre">Sobre</Link>
+          <Link to="/servicos">Serviços</Link>
           <Link to="/contato">Contato</Link>
         </nav>
         
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contato" element={<Contato />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/servicos" element={<Servicos />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="*" element={<div><h1>404 - Página não encontrada</h1></div>} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
